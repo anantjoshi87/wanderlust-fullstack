2152386e2fe8eefeba892510c542e508e4e4dd07
anantjoshi87 <anantji8767@gmail.com>
2026-02-07 17:14:45 +0530

    initialising first full stack app

---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: String,
+
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1568605114967-8130f3a36994?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  },
+
+  price: Number,
+  location: String,
+  country: String,
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
